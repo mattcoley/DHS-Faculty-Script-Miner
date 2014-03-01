5ef3448caf09df7eb2eac13100c164710b3ac76c
mattcoley <mattcoley@optonline.net>
2014-03-01 14:28:30 -0500

    Fixes with gathering information
    Script successfully gathers information of teachers without any bugs.
    MySQL database connection will be implemented next.

--- DHSScript.py
+++ DHSScript.py
@@ -8,9 +8,8 @@ html = response.read()
 cut = html.index('<td style="border-bottom-width: 1px; color: #0000ff; padding: 6px 8px;" align="center" valign="top">Department</td>')
 html = html[cut + 150:]
 
-#html = '</tr><tr><td style="border-bottom-width: 1px; color: #0000ff; padding: 6px 8px;" align="center" valign="top"><span style="color: #0000ff;"> Antunes</span></td>'
 
-
+#Loop through the table
 while html.__len__() > 1500:
     val = 'valign="top">'
     start = html.index(val) + val.__len__()
@@ -25,6 +24,7 @@ while html.__len__() > 1500:
     phone = ""
     office = ""
     department = ""
+    special = ""
 
 
     #TO GET THE LINKS AND LAST NAME
@@ -50,15 +50,15 @@ while html.__len__() > 1500:
             lastName = temp[beg:end]
 
             html = html[val.__len__():]
-
+    else:
+        lastName = temp + ""
 
     #TO GET THE FIRST NAME
-
     val = 'valign="top">'
     end = '</td>'
 
     html = html[html.index(val) + val.__len__():]
-    if (html[0:5] == '<span'):
+    if html[0:5] == '<span':
         val = '#0000ff;">'
         html = html[html.index(val) + val.__len__():]
         end = '</span>'
@@ -73,12 +73,25 @@ while html.__len__() > 1500:
     html = html[html.index(val) + val.__len__():]
     temp = html[:html.index('</td>')]
     phone = temp
+    if '-' in phone:
+        phone = ''
+
+    phone.replace(' ', '')
+
+    while '<' in phone:
+        fIn = phone.index('<')
+        fTwo = phone.index('>')
+        phone = phone[:fIn] + phone[fTwo + 1:]
 
     #TO GET EMAIL
     firstName = firstName.replace(" ", "")
     lastName = lastName.replace(" ", "")
 
-    email = firstName[0].lower() + lastName + "@darienps.org"
+    adjust = lastName
+    if '-' in adjust:
+        adjust = adjust[:adjust.index('-')]
+
+    email = firstName[0].lower() + adjust + "@darienps.org"
     val = 'valign="top">'
 
     html = html[html.index(val) + val.__len__():]
@@ -90,6 +103,14 @@ while html.__len__() > 1500:
     temp = html[:html.index('</td>')]
     office = temp
     office = office.replace(" ", "")
+    office = office.strip()
+    if '-' in office:
+        office = ''
+
+    while '<' in office:
+        fIn = office.index('<')
+        fTwo = office.index('>')
+        office = office[:fIn] + office[fTwo + 1:]
 
     #TO GET DEPARTMENT
     val = 'valign="top">'
@@ -102,17 +123,42 @@ while html.__len__() > 1500:
         end = '</span>'
 
     temp = html[:html.index(end)]
+    while '<' in temp:
+        fIn = temp.index('<')
+        fTwo = temp.index('>')
+        temp = temp[:fIn] + temp[fTwo + 1:]
+
+    if 'Coordinator' in temp:
+        temp = temp.replace('Coordinator', '')
+        special = 'Coordinator'
+    elif 'Secretary' in temp:
+        temp = temp.replace('Secretary', '')
+        special = 'Secretary'
+
+    temp = temp.replace('-', '')
+    if '&amp;' in temp:
+        temp = temp[:temp.index('&amp')]
+
+    temp = temp.strip()
+
     department = temp
 
 
-
-
-
+    #Print out results
     print("******************************")
-    print(link)
-    print(lastName)
-    print(firstName)
-    print(phone)
-    print(email)
-    print(office)
-    print(department)
+    if link.__len__() != 0:
+        print(link)
+    if lastName.__len__() != 0:
+        print(lastName)
+    if firstName.__len__() != 0:
+        print(firstName)
+    if phone.__len__() != 0:
+        print(phone)
+    if email.__len__() != 0:
+        print(email)
+    if office.__len__() != 0:
+        print(office)
+    if department.__len__() != 0:
+        print(department)
+    if special.__len__() != 0:
+        print(special)
